c677df0a91d782dd8fe80dfc0168658277247aa4
joynul24 <joynula919@gmail.com>
2025-12-28 15:43:06 +0600

    I have added home page in balance and logout content

--- js/login.js
+++ js/login.js
@@ -3,7 +3,8 @@ document.getElementById("login-btn").addEventListener("click", function(e){
     const phoneNumber = document.getElementById("phone-number").value;
     const inputPin = document.getElementById("input-pin").value;
     if(phoneNumber === "" || inputPin === "") {
-        alert("Please fill in all fields!")
+        alert("Please fill in all fields!");
+        return;
     }
     if(phoneNumber.length <= 11 && inputPin === "1234") {
         window.location.href = "./home.html"
