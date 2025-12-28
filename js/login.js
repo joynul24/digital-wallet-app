bb9226d22b070b753c0b68312c73a669cfc3b6e8
joynul24 <joynula919@gmail.com>
2025-12-28 12:44:40 +0600

    I have added my digital wallet app simple structure

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById("login-btn").addEventListener("click", function(e){
+    e.preventDefault()
+    const phoneNumber = document.getElementById("phone-number").value;
+    const inputPin = document.getElementById("input-pin").value;
+    if(phoneNumber === "" || inputPin === "") {
+        alert("Please fill in all fields!")
+    }
+    if(phoneNumber.length <= 11 && inputPin === "1234") {
+        window.location.href = "../home.html"
+    }else{
+       alert("Wrond Youre Password or Pin!")
+    }
+})
